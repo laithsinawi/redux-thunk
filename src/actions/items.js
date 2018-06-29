92f0556717fc79deb8d486673b40ff4c749337d9
Laith Sinawi <laith@laithsinawi.com>
2018-06-28 17:43:21 -0700

    build action creators

---
+++ src/actions/items.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+import { ITEMS_HAS_ERRORED, ITEMS_IS_LOADING, ITEMS_FETCH_DATA_SUCCESS } from './types';
+
+export const itemsHasErrored = bool => ({
+    type: ITEMS_HAS_ERRORED,
+    hasErrored: bool
+});
+
+export const itemsIsLoading = bool => ({
+    type: ITEMS_IS_LOADING,
+    isLoading: bool
+});
+
+export const itemsFetchDataSuccess = items => ({
+    type: ITEMS_FETCH_DATA_SUCCESS,
+    items
+});
+
+export const itemsFetchData = url => {
+    return(dispatch) => {
+
+        dispatch(itemsIsLoading(true));
+
+        axios.get('http://5b356e716005b00014c5dc00.mockapi.io/items')
+            .then(res => {
+                const items = res.data;
+                dispatch(itemsIsLoading(false));
+                dispatch(itemsFetchDataSuccess(items));
+            });
+    }
+}
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -12,30 +12,7 @@ export default class ItemList extends Component {
     }
 
     componentDidMount(){
-        this.fetchData('http://5b356e716005b00014c5dc00.mockapi.io/items')
-    }
 
-    fetchData(url) {
-        this.setState({
-            isLoading: true
-        });
-
-        fetch(url)
-        .then( response => {
-            if(!response.ok){
-                throw Error(response.statusText)
-            }
-
-            this.setState({
-                isLoading: false
-            });
-
-            return response;
-        })
-        .then( response => response.json())
-        .then((items) => {
-            this.setState({ items })
-        })
     }
 
     render() {
